d85c6bde35f151dd846f7799d806f59388b5bad8
everuthingaud <setiadar@bc.edu>
2017-07-14 12:20:48 -0700

    m

--- snackchat/components/loginScreen.js
+++ snackchat/components/loginScreen.js
@@ -1,44 +1,74 @@
 import React from 'react';
 import styles from '../styling/styles';
 import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  ListView,
-  Alert,
-  Button,
-  StyleSheet
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput,
+    ListView,
+    Alert,
+    Button,
+    StyleSheet
 } from 'react-native';
 
 //Screens
 class LoginScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Login'
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'Username',
+            password: 'Password',
+        }
+    };
 
-  press() {
+    static navigationOptions = {
+        title: 'Login'
+    };
 
-  }
-  register() {
-    this.props.navigation.navigate('Register');
-  }
+    login() {
 
-  render() {
-    return (
-      <View style={{marginTop: '50%'}}>
-        <Text style={[styles.textBig, styles.backgroundWhite]}>SnackChat!</Text>
+    }
+    register() {
+        this.props.navigation.navigate('Register');
+    }
 
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
-          <Text style={styles.buttonLabel}>Tap to Login</Text>
-        </TouchableOpacity>
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.loginBkg}>
+                    <Image source={require('../assets/image/logo.png')} />
+                    <Text style={styles.textBig}>SnackChat!</Text>
+                </View>
+                <View style={style.loginContainer}>
+                    {/*LOGIN*/}
+                    <TextInput style={styles.inputField}
+                        onChangeText={(text) => this.setState({username: text})}
+                        value={this.state.username}
+                    />
+                    {/*PASSWORD*/}
+                    <TextInput style={styles.inputField}
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
+                    />
+                    <View>
+                        <TouchableOpacity onPress={ () => {this.login()} } style={style.loginContainer}>
+                            <Text style={styles.}>Login</Text>
+                        </TouchableOpacity>
+                    </View>
 
-        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
-          <Text style={styles.buttonLabel}>Tap to Register</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+
+
+                </View>
+                <TouchableOpacity onPress={ () => {this.press()} } style={{}}>
+                    <Text style={{}}>Tap to Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{}} onPress={ () => {this.register()} }>
+                    <Text style={{}}>Tap to Register</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
 export default LoginScreen
--- snackchat/styling/styles.js
+++ snackchat/styling/styles.js
@@ -3,59 +3,78 @@ import {StyleSheet} from 'react-native';
 
 //Styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FC43',
-  },
-  containerFull: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  textBig: {
-    fontSize: 36,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    alignSelf: 'stretch',
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    borderRadius: 10
-  },
-  buttonRed: {
-    backgroundColor: '#FF585B',
-  },
-  buttonBlue: {
-    backgroundColor: '#0074D9',
-  },
-  buttonGreen: {
-    backgroundColor: '#2ECC40'
-  },
-  buttonLabel: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: 'white'
-  },
-  backgroundWhite: {
-    backgroundColor: "white"
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    containerFull: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    textBig: {
+        fontSize: 36,
+        textAlign: 'center',
+        margin: 10,
+    },
+    button: {
+        alignSelf: 'stretch',
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10
+    },
+    buttonRed: {
+        backgroundColor: '#FF585B',
+    },
+    buttonBlue: {
+        backgroundColor: '#0074D9',
+    },
+    buttonGreen: {
+        backgroundColor: '#2ECC40'
+    },
+    buttonLabel: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: 'white'
+    },
+    backgroundWhite: {
+        backgroundColor: "white"
+    },
+    inputField: {
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: 'black',
+        fontFamily: 'Cochin'
+    },
+    secondaryButton: {
+
+    },
+    loginContainer: {
+        backgroundColor: "#99d5f5",
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+    loginBkg: {
+        backgroundColor: "#f0c3d2",
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+
 });
 
 export default styles;
