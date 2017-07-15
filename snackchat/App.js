6bc9dc4dd51ad12d0e2fa877a3eaf4f76bf0b354
Carlie Ostrom <rlietrom@gmail.com>
2017-07-14 18:03:35 -0700

    hey

--- snackchat/App.js
+++ snackchat/App.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import styles from './styling/styles';
 import LoginScreen from './components/loginScreen';
 import RegisterScreen from './components/registerScreen';
+import FriendsScreen from './components/friendsScreen';
+import ViewSnacksScreen from './components/viewSnacksScreen';
 import { StackNavigator } from 'react-navigation';
+
 
 export default StackNavigator({
     Login: {
@@ -20,5 +23,19 @@ export default StackNavigator({
         title: 'Register'
       }
     },
+    FriendsScreen: {
+      screen: FriendsScreen,
+      navigationOptions:  {
+        header: null,
+        title: 'Friends Screen'
+      }
+    },
+    ViewSnacksScreen: {
+      screen: ViewSnacksScreen,
+      navigationOptions:  {
+        header: null,
+        title: 'View Snacks Screen'
+      }
+    }
 }, {headerMode: 'screen',
-    initialRouteName: 'Login'});
+    initialRouteName: 'FriendsScreen'});
