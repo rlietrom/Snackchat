fc4463831d8c2a538659ae934cce39b7ca1029aa
Evan <Evanp1324@gmail.com>
2017-07-15 03:52:57 -0700

    m

--- snackchat/components/loginScreen.js
+++ snackchat/components/loginScreen.js
@@ -13,47 +13,6 @@ import {
   AsyncStorage
 } from 'react-native';
 import * as Animatable from 'react-native-animatable'
-/**
- * TESTING BELOW,,,,,,,,,,
- */
-import NutritionFacts from 'nutrition-facts'
-
-const NF = new NutritionFacts('hrbPZg66gjAlAMTsyJ20csfGqevaf8FEWtyNr1Rg');
-NF.searchFoods({
-  q: 'salted butter',
-  ds: 'Standard Reference'
-}).then(results => {
-
-  // Returns search results
-  let mySelectedItem = results.list.item[0]
-
-  // Items are returned as a FoodItem instance
-  // allowing you to call 'getNutrition' directly on the instance.
-  mySelectedItem.getNutrition()
-  .then(nutritionReport => {
-    console.log(nutritionReport)
-  }).catch(err => {
-    console.log(err)
-  })
-
-}).catch(err => {
-  console.log(err)
-})
-
-// Alternatively, if you know the NDBNO off-hand
-// you can call 'getNutrition' from the NF instance.
-
-NF.getNutrition('01001','b')
-.then(nutritionReport => {
-  console.log(nutritionReport)
-}).catch(err => {
-  console.log(err)
-})
-
-})
-/**
- * TESTING ABOVE ^^^^^^^^^^^
- */
 //Screens
 class LoginScreen extends React.Component {
   constructor(props) {
